9f2714a41285afffd7a5aafe919543df1ef6eeb4
gwyn <gwyn.gras.usry@gmail.com>
2025-04-20 13:37:43 -0700

    app icon added

--- main.cpp
+++ main.cpp
@@ -7,8 +7,10 @@ int main(int argc, char *argv[])
 {
     qRegisterMetaType<MealPlan>("MealPlan");
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":/icon.jpg"));
     MainWindow w;
     w.show();
     return a.exec();
 
 }
+
